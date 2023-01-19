aee69c1cd16dd54e7c860b86aae906f99ba32e99
Thomas Liu <79129497+thomasxliu@users.noreply.github.com>
2023-01-19 18:58:01 -0500

    Add exploreTopBooks component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import { ExploreTopBooks } from './layout/HomePage/ExploreTopBooks';
 import { Navbar } from './layout/NavbarAndFooter/Navbar';
 
 function App() {
   return (
-    <Navbar />
+    <div>
+      <Navbar />
+      <ExploreTopBooks />
+    </div>
   );
 }
 

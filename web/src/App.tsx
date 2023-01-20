56a49337fcf543c02ebe6a863810a01280ce2a28
Thomas Liu <79129497+thomasxliu@users.noreply.github.com>
2023-01-20 17:09:30 -0500

    add book carousel component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import './App.css';
-import { ExploreTopBooks } from './layout/HomePage/ExploreTopBooks';
-import { Navbar } from './layout/NavbarAndFooter/Navbar';
+import React from "react";
+import "./App.css";
+import { Carousel } from "./layout/HomePage/Carousel";
+import { ExploreTopBooks } from "./layout/HomePage/ExploreTopBooks";
+import { Navbar } from "./layout/NavbarAndFooter/Navbar";
 
 function App() {
   return (
     <div>
       <Navbar />
       <ExploreTopBooks />
+      <Carousel />
     </div>
   );
 }

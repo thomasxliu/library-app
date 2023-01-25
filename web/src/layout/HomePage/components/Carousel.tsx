5be4c3814e4748b45366c56edc5b8e73bade525c
Thomas Liu <79129497+thomasxliu@users.noreply.github.com>
2023-01-25 16:33:22 -0500

    using fetched data instead of hardcoded data

--- web/src/layout/HomePage/components/Carousel.tsx
+++ web/src/layout/HomePage/components/Carousel.tsx
@@ -81,25 +81,25 @@ export const Carousel = () => {
         <div className="carousel-inner">
           <div className="carousel-item active">
             <div className="row d-flex justify-content-center align-items-center">
-              <ReturnBook />
-              <ReturnBook />
-              <ReturnBook />
+              {books
+                .slice(0, 3)
+                .map(book => <ReturnBook book={book} key={book.id} />)}
             </div>
           </div>
 
           <div className="carousel-item">
             <div className="row d-flex justify-content-center align-items-center">
-              <ReturnBook />
-              <ReturnBook />
-              <ReturnBook />
+              {books
+                .slice(3, 6)
+                .map(book => <ReturnBook book={book} key={book.id} />)}
             </div>
           </div>
 
           <div className="carousel-item">
             <div className="row d-flex justify-content-center align-items-center">
-              <ReturnBook />
-              <ReturnBook />
-              <ReturnBook />
+              {books
+                .slice(6, 9)
+                .map(book => <ReturnBook book={book} key={book.id} />)}
             </div>
           </div>
         </div>
@@ -128,7 +128,7 @@ export const Carousel = () => {
       {/* Mobile */}
       <div className="d-lg-none mt-3">
         <div className="row d-flex justify-content-center align-items-center">
-          <ReturnBook />
+          <ReturnBook book={books[7]} />
         </div>
       </div>
       <div className="homepage-carousel-title mt-3">

48f0b0611f6cf8ce96e17cf2c6c65d5699845e57
Thomas Liu <79129497+thomasxliu@users.noreply.github.com>
2023-01-25 20:53:31 -0500

    add SearchBooksPage

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,12 +4,14 @@ import "./App.css";
 import { HomePage } from "./layout/HomePage/HomePage";
 import { Footer } from "./layout/NavbarAndFooter/Footer";
 import { Navbar } from "./layout/NavbarAndFooter/Navbar";
+import { SearchBooksPage } from "./layout/SearchBookPage/SearchBooksPage";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <HomePage />
+      {/* <HomePage /> */}
+      <SearchBooksPage />
       <Footer />
     </div>
   );

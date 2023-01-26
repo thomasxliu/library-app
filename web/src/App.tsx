dff1f3d9598a940bd9396e1e28b4c7f1572cfc90
Thomas Liu <79129497+thomasxliu@users.noreply.github.com>
2023-01-26 17:31:54 -0500

    add routing

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 import { HomePage } from "./layout/HomePage/HomePage";
 import { Footer } from "./layout/NavbarAndFooter/Footer";
@@ -10,8 +11,17 @@ function App() {
   return (
     <div>
       <Navbar />
-      {/* <HomePage /> */}
-      <SearchBooksPage />
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/home" />
+        </Route>
+        <Route path="/home">
+          <HomePage />
+        </Route>
+        <Route path="/search">
+          <SearchBooksPage />
+        </Route>
+      </Switch>
       <Footer />
     </div>
   );

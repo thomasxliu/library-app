f57dc92a97ee0ffebbf9ed0cb049b7c2eab878f3
Thomas Liu <79129497+thomasxliu@users.noreply.github.com>
2023-01-28 18:39:33 -0500

    currentLoansCount added

--- web/src/layout/BookCheckoutPage/BookCheckoutPage.tsx
+++ web/src/layout/BookCheckoutPage/BookCheckoutPage.tsx
@@ -1,4 +1,6 @@
+import { useOktaAuth } from "@okta/okta-react";
 import { useEffect, useState } from "react";
+
 import BookModel from "../../models/BookModel";
 import ReviewModel from "../../models/ReviewModel";
 import { SpinnerLoading } from "../Utils/SpinnerLoading";
@@ -7,6 +9,8 @@ import { CheckoutAndReviewBox } from "./components/CheckoutAndReviewBox";
 import { LatestReviews } from "./components/LatestReviews";
 
 export const BookCheckoutPage = () => {
+  const { authState } = useOktaAuth();
+
   const [book, setBook] = useState<BookModel>();
   const [isLoading, setIsLoading] = useState(true);
   const [httpError, setHttpError] = useState(null);
@@ -17,6 +21,11 @@ export const BookCheckoutPage = () => {
   const [isLoadingReview, setIsLoadingReview] = useState(true);
 
   const bookId = window.location.pathname.split("/")[2];
+
+  // loans count state
+  const [currentLoansCount, setCurrentLoansCount] = useState(0);
+  const [isLoadingCurrentLoansCount, setIsLoadingCurrentLoansCount] =
+    useState(true);
 
   useEffect(() => {
     const fetchBook = async () => {
@@ -38,7 +47,7 @@ export const BookCheckoutPage = () => {
         copies: responseJson.copies,
         copiesAvailable: responseJson.copiesAvailable,
         category: responseJson.category,
-        img: responseJson.img
+        img: responseJson.img,
       };
 
       setBook(loadedBook);
@@ -52,59 +61,86 @@ export const BookCheckoutPage = () => {
 
   useEffect(() => {
     const fetchBookReviews = async () => {
-        const reviewUrl: string = `http://localhost:8080/api/reviews/search/findByBookId?bookId=${bookId}`;
+      const reviewUrl: string = `http://localhost:8080/api/reviews/search/findByBookId?bookId=${bookId}`;
 
-        const responseReviews = await fetch(reviewUrl);
+      const responseReviews = await fetch(reviewUrl);
 
-        if (!responseReviews.ok) {
-            throw new Error('Something went wrong!');
-        }
+      if (!responseReviews.ok) {
+        throw new Error("Something went wrong!");
+      }
 
-        const responseJsonReviews = await responseReviews.json();
+      const responseJsonReviews = await responseReviews.json();
 
-        const responseData = responseJsonReviews._embedded.reviews;
+      const responseData = responseJsonReviews._embedded.reviews;
 
-        const loadedReviews: ReviewModel[] = [];
+      const loadedReviews: ReviewModel[] = [];
 
-        let weightedStarReviews: number = 0;
+      let weightedStarReviews: number = 0;
 
-        for (const key in responseData) {
-            loadedReviews.push({
-                id: responseData[key].id,
-                userEmail: responseData[key].userEmail,
-                date: responseData[key].date,
-                rating: responseData[key].rating,
-                book_id: responseData[key].bookId,
-                reviewDescription: responseData[key].reviewDescription,
-            });
-            weightedStarReviews = weightedStarReviews + responseData[key].rating;
-        }
+      for (const key in responseData) {
+        loadedReviews.push({
+          id: responseData[key].id,
+          userEmail: responseData[key].userEmail,
+          date: responseData[key].date,
+          rating: responseData[key].rating,
+          book_id: responseData[key].bookId,
+          reviewDescription: responseData[key].reviewDescription,
+        });
+        weightedStarReviews = weightedStarReviews + responseData[key].rating;
+      }
 
-        if (loadedReviews) {
-            const round = (Math.round((weightedStarReviews / loadedReviews.length) * 2) / 2).toFixed(1);
-            setTotalStars(Number(round));
-        }
+      if (loadedReviews) {
+        const round = (
+          Math.round((weightedStarReviews / loadedReviews.length) * 2) / 2
+        ).toFixed(1);
+        setTotalStars(Number(round));
+      }
 
-        setReviews(loadedReviews);
-        setIsLoadingReview(false);
+      setReviews(loadedReviews);
+      setIsLoadingReview(false);
     };
 
     fetchBookReviews().catch((error: any) => {
-        setIsLoadingReview(false);
-        setHttpError(error.message);
-    })
-}, []);
+      setIsLoadingReview(false);
+      setHttpError(error.message);
+    });
+  }, []);
 
-  if (isLoading || isLoadingReview) {
+  useEffect(() => {
+    const fetchUserCurrentLoansCount = async () => {
+      if (authState && authState.isAuthenticated) {
+        const url = `http://localhost:8080/api/books/secure/currentloans/count`;
+        const requestOptions = {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${authState.accessToken?.accessToken}`,
+            "Content-Type": "application/json",
+          },
+        };
+        const currentLoansCountResponse = await fetch(url, requestOptions);
+        if (!currentLoansCountResponse.ok) {
+          throw new Error("Something went wrong!");
+        }
+        const currentLoansCountResponseJson =
+          await currentLoansCountResponse.json();
+        setCurrentLoansCount(currentLoansCountResponseJson);
+      }
+      setIsLoadingCurrentLoansCount(false);
+    };
+    fetchUserCurrentLoansCount().catch((error: any) => {
+      setIsLoadingCurrentLoansCount(false);
+      setHttpError(error.message);
+    });
+  }, [authState]);
+
+  if (isLoading || isLoadingReview || isLoadingCurrentLoansCount) {
     return <SpinnerLoading />;
   }
 
   if (httpError) {
     return (
       <div className="container m-5">
-        <p>
-          {httpError}
-        </p>
+        <p>{httpError}</p>
       </div>
     );
   }
@@ -114,7 +150,16 @@ export const BookCheckoutPage = () => {
       <div className="container d-none d-lg-block">
         <div className="row mt-5">
           <div className="col-sm-2 col-md-2">
-            {book?.img ? <img src={book?.img} width="226" height="349" alt="Book" /> : <img src={require("./../../Images/BooksImages/book-luv2code-1000.png")} width="226" height="349" alt="Book" /> }
+            {book?.img ? (
+              <img src={book?.img} width="226" height="349" alt="Book" />
+            ) : (
+              <img
+                src={require("./../../Images/BooksImages/book-luv2code-1000.png")}
+                width="226"
+                height="349"
+                alt="Book"
+              />
+            )}
           </div>
           <div className="col-4 col-md-4 container">
             <div className="ml-2">
@@ -124,24 +169,41 @@ export const BookCheckoutPage = () => {
               <StarsReview rating={totalStars} size={32} />
             </div>
           </div>
-          <CheckoutAndReviewBox book={book} mobile={false} />
+          <CheckoutAndReviewBox
+            book={book}
+            mobile={false}
+            currentLoansCount={currentLoansCount}
+          />
         </div>
         <hr />
         <LatestReviews reviews={reviews} bookId={book?.id} mobile={false} />
       </div>
       <div className="container d-lg-none mt-5">
         <div className="d-flex justify-content-center align-item-center">
-          {book?.img ? <img src={book?.img} width="226" height="349" alt="Book" /> : <img src={require("./../../Images/BooksImages/book-luv2code-1000.png")} width="226" height="349" alt="Book" /> }
+          {book?.img ? (
+            <img src={book?.img} width="226" height="349" alt="Book" />
+          ) : (
+            <img
+              src={require("./../../Images/BooksImages/book-luv2code-1000.png")}
+              width="226"
+              height="349"
+              alt="Book"
+            />
+          )}
         </div>
         <div className="mt-4">
           <div className="ml-2">
-              <h2>{book?.title}</h2>
-              <h5 className="text-primary">{book?.author}</h5>
-              <p className="lead">{book?.description}</p>
-              <StarsReview rating={totalStars} size={32} />
+            <h2>{book?.title}</h2>
+            <h5 className="text-primary">{book?.author}</h5>
+            <p className="lead">{book?.description}</p>
+            <StarsReview rating={totalStars} size={32} />
           </div>
         </div>
-        <CheckoutAndReviewBox book={book} mobile={true} />
+        <CheckoutAndReviewBox
+          book={book}
+          mobile={true}
+          currentLoansCount={currentLoansCount}
+        />
         <hr />
         <LatestReviews reviews={reviews} bookId={book?.id} mobile={true} />
       </div>
--- web/src/layout/BookCheckoutPage/components/CheckoutAndReviewBox.tsx
+++ web/src/layout/BookCheckoutPage/components/CheckoutAndReviewBox.tsx
@@ -1,17 +1,31 @@
 import { Link } from "react-router-dom";
 import BookModel from "../../../models/BookModel";
 
-export const CheckoutAndReviewBox: React.FC<{book: BookModel | undefined, mobile: boolean}> = (props) => {
+export const CheckoutAndReviewBox: React.FC<{
+  book: BookModel | undefined;
+  mobile: boolean;
+  currentLoansCount: number;
+}> = (props) => {
   return (
-    <div className={props.mobile ? "card d-flex mt-5" : "card col-3 container d-flex mb-5"}>
+    <div
+      className={
+        props.mobile ? "card d-flex mt-5" : "card col-3 container d-flex mb-5"
+      }
+    >
       <div className="card-body container">
         <div className="mt-3">
           <p>
-            <b>0/5 </b>
+            <b>{props.currentLoansCount}/5 </b>
             books checkout out
           </p>
           <hr />
-          {props.book && props.book.copiesAvailable && props.book.copiesAvailable > 0 ? <h4 className="text-sucess">Available</h4> : <h4 className="text-danger">Wait List</h4>}
+          {props.book &&
+          props.book.copiesAvailable &&
+          props.book.copiesAvailable > 0 ? (
+            <h4 className="text-sucess">Available</h4>
+          ) : (
+            <h4 className="text-danger">Wait List</h4>
+          )}
           <div className="row">
             <p className="col-6 lead">
               <b>{props.book?.copies} </b>
@@ -23,11 +37,15 @@ export const CheckoutAndReviewBox: React.FC<{book: BookModel | undefined, mobile
             </p>
           </div>
         </div>
-        <Link to="/#" className="btn btn-success btn-lg">Sign in</Link>
+        <Link to="/#" className="btn btn-success btn-lg">
+          Sign in
+        </Link>
         <hr />
-        <p className="mt-3">This number can change until placing order has been complete.</p>
+        <p className="mt-3">
+          This number can change until placing order has been complete.
+        </p>
         <p>Sign in to be able to leave a review.</p>
       </div>
     </div>
   );
-}
+};

8e1423c812b454b672c5dc79bc12fabb49936a13
Thomas Liu <79129497+thomasxliu@users.noreply.github.com>
2023-01-28 01:14:25 -0500

    refractor home page

--- web/src/layout/HomePage/components/ReturnBook.tsx
+++ web/src/layout/HomePage/components/ReturnBook.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import BookModel from "../../../models/BookModel";
 
 export const ReturnBook: React.FC<{ book: BookModel }> = props => {
@@ -20,9 +21,12 @@ export const ReturnBook: React.FC<{ book: BookModel }> = props => {
         <p>
           {props.book.author}
         </p>
-        <a className="btn main-color text-white" href="#">
+        <Link
+          className="btn main-color text-white"
+          to={`checkout/${props.book.id}`}
+        >
           Reserve
-        </a>
+        </Link>
       </div>
     </div>
   );

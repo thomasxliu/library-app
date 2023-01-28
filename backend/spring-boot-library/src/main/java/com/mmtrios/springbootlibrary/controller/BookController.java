d805fcf7fb2c65f289a7d5cb643adb247512919c
Thomas Liu <79129497+thomasxliu@users.noreply.github.com>
2023-01-28 18:15:43 -0500

    add jwt extraction method

--- backend/spring-boot-library/src/main/java/com/mmtrios/springbootlibrary/controller/BookController.java
+++ backend/spring-boot-library/src/main/java/com/mmtrios/springbootlibrary/controller/BookController.java
@@ -2,10 +2,12 @@ package com.mmtrios.springbootlibrary.controller;
 
 import com.mmtrios.springbootlibrary.entity.Book;
 import com.mmtrios.springbootlibrary.service.BookService;
+import com.mmtrios.springbootlibrary.utils.ExtractJWT;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,20 +24,22 @@ public class BookController {
     }
 
     @PutMapping("/secure/checkout")
-    public Book checkoutBook(@RequestParam Long bookId) throws Exception{
-        String userEmail = "testuser@mmtrios.com";
+    public Book checkoutBook(@RequestParam Long bookId, @RequestHeader(value="Authorization") String token) throws Exception{
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
         return bookService.checkoutBook(userEmail, bookId);
     }
 
     @GetMapping("/secure/currentloans/count")
-    public int currentLoansCount(){
-        String userEmail = "testuser@mmtrios.com";
+    public int currentLoansCount(@RequestHeader(value="Authorization") String token){
+
+
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
         return bookService.currentLoansCount(userEmail);
     }
 
     @GetMapping("/secure/ischeckedout/byuser")
-    public Boolean checkoutBookByUser(@RequestParam Long bookId){
-        String userEmail = "testuser@mmtrios.com";
+    public Boolean checkoutBookByUser(@RequestParam Long bookId, @RequestHeader(value="Authorization") String token){
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
         return bookService.checkoutBookByUser(userEmail, bookId);
     }
 

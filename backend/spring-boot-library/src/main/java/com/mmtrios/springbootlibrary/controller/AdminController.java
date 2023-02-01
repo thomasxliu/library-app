5f42a258cb775e01073b1e27ad3681092e13bfae
Thomas Liu <79129497+thomasxliu@users.noreply.github.com>
2023-01-31 21:08:50 -0500

    add book backend support done

---
+++ backend/spring-boot-library/src/main/java/com/mmtrios/springbootlibrary/controller/AdminController.java
@@ -0,0 +1,34 @@
+package com.mmtrios.springbootlibrary.controller;
+
+import com.mmtrios.springbootlibrary.requestmodels.AddBookRequest;
+import com.mmtrios.springbootlibrary.service.AdminService;
+import com.mmtrios.springbootlibrary.utils.ExtractJWT;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/api/admin")
+public class AdminController {
+    private AdminService adminService;
+
+    @Autowired
+    public AdminController(AdminService adminService){
+        this.adminService = adminService;
+    }
+
+    @PostMapping("/secure/add/book")
+    public void postBook(@RequestHeader(value="Authorization") String token,
+            @RequestBody AddBookRequest addBookRequest) throws Exception {
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+        if (admin == null || !admin.equals("admin")) {
+            throw new Exception("Administration page only");
+        }
+        adminService.postBook(addBookRequest);
+    }
+}
---
+++ backend/spring-boot-library/src/main/java/com/mmtrios/springbootlibrary/service/AdminService.java
@@ -0,0 +1,31 @@
+package com.mmtrios.springbootlibrary.service;
+
+import com.mmtrios.springbootlibrary.dao.BookRepository;
+import com.mmtrios.springbootlibrary.entity.Book;
+import com.mmtrios.springbootlibrary.requestmodels.AddBookRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class AdminService {
+    private BookRepository bookRepository;
+
+    @Autowired
+    public AdminService (BookRepository bookRepository){
+        this.bookRepository = bookRepository;
+    }
+
+    public void postBook(AddBookRequest addBookRequest) {
+        Book book = new Book();
+        book.setTitle(addBookRequest.getTitle());
+        book.setAuthor(addBookRequest.getAuthor());
+        book.setDescription(addBookRequest.getDescription());
+        book.setCopies(addBookRequest.getCopies());
+        book.setCopiesAvailable(addBookRequest.getCopies());
+        book.setCategory(addBookRequest.getCategory());
+        book.setImg(addBookRequest.getImg());
+        bookRepository.save(book);
+    }
+}
